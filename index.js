a5572d021f744ab191987a0706fe8f2a633d8d7d
robin nganga smith <saintofcookies.2021@gmail.com>
2023-09-25 19:09:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+let result;
+if (someNumber <=400) {
+result = 'This one is on me!'
+}else if (someNumber >2000 && someNumber < 2500) {
+  result = 'I will gladly take your thirty bucks.'
+}else if (someNumber > 2500) {
+result = 'No can do.'
+}
+return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+ return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+    switch (tip) {
+      case 'generous':return "Thank you so much.";
+       case 'not as generous':return "Thank you.";
+      default :return 'Bye.';
+    }
+  }
